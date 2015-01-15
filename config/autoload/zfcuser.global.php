5a2ea133a578a45754306b1321b80e4ad438d09c
schwii <jegi@swissonline.ch>
2015-01-15 19:24:01 +0100

    1

--- config/autoload/zfcuser.global.php
+++ config/autoload/zfcuser.global.php
@@ -247,7 +247,7 @@ $settings = array(
      * Include null if you want user's with no state to login as well.
      * Allowed value types: null and integer
      */
-    'allowed_login_states' => array( null, 1 ),
+    'allowed_login_states' => array( null, 1,3 ),
     
     /**
      * User table name
