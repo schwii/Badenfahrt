b77ac66f902da9524de9e1d073c550ad2cdda264
schwii <jegi@swissonline.ch>
2015-01-22 11:28:55 +0100

    1

--- module/Backend/view/Backend/Backend/editUserView.php
+++ module/Backend/view/Backend/Backend/editUserView.php
@@ -1,63 +1,71 @@
 <?php
 
-class editUserView{
+class editUserView {
 
-public function editUser($entityManager, $userID){
+    protected $admin;
     
-     $user = $entityManager->find('Backend\Entity\User', $userID);
-    $email = $user->getEmail();
-    $contactLast = $user->getContactLast();
-    $contactSur = $user->getContactSur();
-    
-    
-    
+    public function editUser($entityManager, $userID) {
+
+        $user = $entityManager->find('Backend\Entity\User', $userID);
+        $email = $user->getEmail();
+        $contactLast = $user->getContactLast();
+        $contactSur = $user->getContactSur();
+
+
+
 //    $user = $entityManager->find('Backend\Entity\User', $userID);
         $orgname = $user->getOrgname();
         $street = $user->getStreet();
         $streetNr = $user->getStreetNr();
         $zip = $user->getZip();
-        $city = $user->getCity();  
+        $city = $user->getCity();
         $image = $user->getLogo();
         $sex = $user->getContactSex();
         $phone = $user->getPhone();
-        $password = $user->getPassword();
-    
+        //$password = $user->getPassword();
+
+
         
-    echo <<<FORM
+        if($this->admin){
+            $passwordInput =  '        <div class="form-group">
+            <label class="col-lg-3 control-label">Passwort:</label>
+            <div class="col-lg-8">
+              <input name="password" class="form-control" type="text">
+            </div>
+          </div>';
+        }
+        else{
+            $passwordInput = "";
+        }
+        
+        
+        echo <<<FORM
 <div class="container">
     <h1>Edit Profile</h1>
   	<hr>
 	<div class="row">
       <!-- left column -->
-      <div class="col-md-3">
-        <div class="text-center">
-          <img src="$image" class="avatar img" alt="avatar">
-          <h6>Anderes Foto hochladen</h6>
-          
-          <input type="file" class="form-control">
-        </div>
-      </div>
+      
       
       <!-- edit form column -->
       <div class="col-md-9 personal-info">
         <div class="alert alert-info alert-dismissable">
-          <a class="panel-close close" data-dismiss="alert">×</a> 
+          <a class="panel-close close" data-dismiss="alert">Schliessens</a> 
           <i class="fa fa-coffee"></i>
-          <strong>Achtung,</strong> hier könnte ihre Werbung stehen!
         </div>
         <h3>Personal info</h3>
-        
-        <form class="form-horizontal" role="form">
+        <form class="form-horizontal" role="form"method="post">
+            <input name="userID" class="form-control" type="hidden" value=$userID >
           <div class="form-group">
             <label class="col-lg-3 control-label">Vorname:</label>
             <div class="col-lg-8">
-              <input class="form-control" type="text" required value=$contactSur >
+              <input name="contactSur" class="form-control" type="text" required value=$contactSur >
             </div>
           </div>
           <div class="form-group">
             <label class="col-lg-3 control-label">Nachname:</label>
             <div class="col-lg-8">
-              <input class="form-control" type="text" required value=$contactLast>
+              <input name="contactLast" class="form-control" type="text" required value=$contactLast>
             </div>
           </div>
          
@@ -66,10 +74,10 @@ public function editUser($entityManager, $userID){
             <label class="col-lg-3 control-label">Geschlecht:</label>
             <div class="col-lg-8">
               <div class="ui-select">
-                <select id="gender" class="form-control">
+                <select name="gender" id="gender" class="form-control">
                   <option {$sex}>Männlich</option>
                   <option {$sex}>Weiblich</option>
-                  <option {$sex}>Keine Angabe</option>
+                  <option {$sex}>Firma</option>
                 </select>
               </div>
             </div>
@@ -78,63 +86,47 @@ public function editUser($entityManager, $userID){
           <div class="form-group">
             <label class="col-lg-3 control-label">Firma, Organisation:</label>
             <div class="col-lg-8">
-              <input class="form-control" type="text" required value=$orgname>
+              <input name="orgname" class="form-control" type="text" value=$orgname>
             </div>
           </div>
               
           <div class="form-group">
             <label class="col-lg-3 control-label">Strasse:</label>
             <div class="col-lg-8">
-              <input class="form-control" type="text" required value=$street >
+              <input name="street" class="form-control" type="text" required value=$street >
             </div>
           </div>  
               
           <div class="form-group">
             <label class="col-lg-3 control-label">Nr:</label>
             <div class="col-lg-8">
-              <input class="form-control" type="text" required value=$streetNr >
+              <input name="streetNr" class="form-control" type="text" required value=$streetNr >
             </div>
           </div>
               
           <div class="form-group">
             <label class="col-lg-3 control-label">Postleitzahl:</label>
             <div class="col-lg-8">
-              <input class="form-control" type="text" required value=$zip >
+              <input name="zip" class="form-control" type="text" required value=$zip >
             </div>
           </div>    
               
               <div class="form-group">
             <label class="col-lg-3 control-label">Ort:</label>
             <div class="col-lg-8">
-              <input class="form-control" type="text" required value=$city >
+              <input name="city" class="form-control" type="text" required value=$city >
             </div>
           </div>
               
              <div class="form-group">
             <label class="col-lg-3 control-label">Telefon:</label>
             <div class="col-lg-8">
-              <input class="form-control" type="text" required value={$this->vars['Phone']} >
+              <input name="phone" class="form-control" type="text" required value=$phone >
             </div>
           </div>   
-          <div class="form-group">
-            <label class="col-lg-3 control-label">Email:</label>
-            <div class="col-lg-8">
-              <input class="form-control" type="text" required value=$email>
-            </div>
-          </div>
          
-          <div class="form-group">
-            <label class="col-md-3 control-label">Passwort:</label>
-            <div class="col-md-8">
-              <input class="form-control" type="password" required value=$password>
-            </div>
-          </div>
-          <div class="form-group">
-            <label class="col-md-3 control-label">Passwort bestätigen:</label>
-            <div class="col-md-8">
-              <input class="form-control" type="password" required value=$password>
-            </div>
-          </div>
+            $passwordInput
+            
           <div class="form-group">
             <label class="col-md-3 control-label"></label>
             <div class="col-md-8">
@@ -150,53 +142,13 @@ public function editUser($entityManager, $userID){
 <hr>
  
 FORM;
-}
-
-public function change(){
-    
-    //Abfragen auf Änderung etc?
-    
-    global $entityManager;
-    $user = $entityManager->find('Backend\Entity\User', $this->vars['userID']);
-    $user->setContactLast($_POST['lastname']);
-    $user->setContactSur($_POST['surname']);
-    $user->setOrgname($_POST['orgname']);
-    $user->setEmail($_POST['email']);
-    $user->setStreet($_POST['street']);
-    $user->setStreetNr($_POST['streetNr']);
-    $user->setZip($_POST['zip']);
-    $user->setCity($_POST['city']);
-    $user->setPhone($_POST['phone']);
-    $user->setTimeCreate($_POST['timecreate']);
-    $user->setLogo($_POST['logo']);
-    $user->setPassword($_POST['password']);
-    
-    if(!$_POST['password']=="" && $_POST['password']==$_POST['passwordrepeat']){
-        $user->setPassword($_POST['password']);
     }
-    else{
-        "echo pw falsch -> bei phtml abfragen und alte werte übergeben";
-    }    
-    
-    $entityManager->flush();
-    
-    //echo $_POST['email'];
-}
-
-public function assign($key, $value) {
-        $this->vars[$key] = $value;
+    public function setAdmin(){
+        $this->admin = true;
     }
-    
-}
-
 
 //include_once('vendor\zendframework\zendframework\library\Zend\Crypt\Password\bcrypt.php'); 
 //$bcrypt = new Zend\Crypt\Password\Bcrypt();
 //$bcrypt->setCost(14);
 //echo $bcrypt->create($password);
-
-
-
-
-
-
+}
